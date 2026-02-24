92a9dfbc18b999cc9e08a81df538262a66280169
inforsegjeferson-ai <inforseg.jeferson@gmail.com>
2026-02-24 20:19:48 -0300

    atualização

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,21 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    allowedHosts: ["acquainted-alona-isocephalic.ngrok-free.dev"],
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      host: "0.0.0.0",
+      port: 3001,
+      allowedHosts: [
+        "192.168.1.52",
+        "jsolar.servepics.com",
+      ],
     },
-  },
-}));
+    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
